316dbdce733ab9f2b80b66768d4962ea62bc6688
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-02-03 17:15:15 +0000

    Give log_transaction STL like interfaces

--- shared/include/log_transaction.hpp
+++ shared/include/log_transaction.hpp
@@ -4,7 +4,20 @@
 
 class log_transaction {
 public:
-	log_transaction(log_utility& base);
+	typedef log_utility utility_type;
+	typedef utility_type::container_type log_type;
+	typedef utility_type::header_type header_type;
+	typedef utility_type::value_type value_type;
+	
+	typedef value_type& reference;
+	typedef const reference const_reference;
+
+	typedef log_type::iterator iterator;
+	typedef log_type::const_iterator const_iterator;
+	
+	
+public:
+	log_transaction(utility_type& base);
 	log_transaction(const log_transaction& orig) = delete;
 	log_transaction(log_transaction&& orig);
 	
@@ -12,17 +25,33 @@ public:
 	
 	log_transaction& operator=(log_transaction&& orig);
 	
-	void write_record(const log_utility::reference record);
+	void write_record(const_reference record);
 	const activity read_record(hash512 hash);
-	hash512 read_hash();
 	
-	const log_header& header(); 
+	const log_header& header();
+	
+	iterator begin();
+	const_iterator cbegin();
+	
+	iterator end();
+	const_iterator cend();
+	
+	reference front();
+	const_reference cfront();
+	
+	reference back();
+	const_reference cback();
 
 private:
+
 	inline void is_valid() {
 		if(!m_valid) throw transaction_invalid_state();
 	};
-	log_utility& m_base;
+
+	utility_type& m_base;
+	log_type& m_log;
+	header_type& m_header;
+	
 	bool m_valid;
 };
 
--- shared/log_transaction.cpp
+++ shared/log_transaction.cpp
@@ -1,12 +1,16 @@
 #include "log_transaction.hpp"
 
-log_transaction::log_transaction(log_utility& base)
+log_transaction::log_transaction(utility_type& base)
 	: m_base(base)
+	, m_log(base.log())
+	, m_header(base.header())
 	, m_valid(true)
 { }
 
 log_transaction::log_transaction(log_transaction&& orig)
 	: m_base(orig.m_base)
+	, m_log(orig.m_log)
+	, m_header(orig.m_header)
 	, m_valid(true)
 {
 	orig.m_valid = false;	
@@ -21,28 +25,56 @@ log_transaction& log_transaction::operator=(log_transaction&& orig) {
 	orig.m_valid = false;
 	
 	m_base = orig.m_base;
+	m_log = orig.m_log;
+	m_header = orig.m_header;
+	
 	m_valid = true;
 	return *this;
 }
 
 const activity log_transaction::read_record(hash512 hash) {
 	is_valid();	
-	auto log = m_base.log();
 }
 
-void log_transaction::write_record(const log_utility::reference record) {
+void log_transaction::write_record(const_reference record) {
 	is_valid();
-	auto log = m_base.log();
-	
-	
-}
-
-hash512 log_transaction::read_hash() {
-	is_valid();
-	
+	m_log.push_back(record);
 }
 
 const log_header& log_transaction::header() {
-	return m_base.header();
+	is_valid();
+	return m_header;
 }
+
+log_transaction::iterator log_transaction::begin() {
+	is_valid();
+	return m_log.begin();
+}
+
+log_transaction::const_iterator log_transaction::cbegin() {
+	is_valid();
+	return m_log.cbegin();
+}
+
+log_transaction::iterator log_transaction::end() {
+	is_valid();
+	return m_log.end();
+}
+
+log_transaction::const_iterator log_transaction::cend() {
+	is_valid();
+	return m_log.cend();
+}
+
+log_transaction::reference log_transaction::back() {
+	is_valid();
+	return m_log.back();
+}
+
+log_transaction::const_reference log_transaction::cback() {
+	is_valid();
+	return m_base.log().back();
+}
+
+
 
