1b5520e965e5c86c55f7ed8bcf31222f8cf01b63
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-02-03 17:15:41 +0000

    Test transaction ctor and dtor

--- testing/shared/mod_activity_log.cpp
+++ testing/shared/mod_activity_log.cpp
@@ -1,8 +1,29 @@
 #include "catch.hpp"
 #include "activity_log.hpp"
 
-TEST_CASE( "Test ctors", "[shared],[activity_log]" ) {
+TEST_CASE( "Test activity_log ctors", "[shared],[activity_log]" ) {
 	SECTION("activity_log Default") {
 		activity_log log;
 	}	
+}
+
+TEST_CASE("Test transaction objects") {
+	activity_log log;
+	
+	SECTION("Basic ctor and dtor") {
+		REQUIRE(log.transacting() == false);
+		
+		{
+			auto t = log.begin_transaction();
+			REQUIRE(log.transacting() == true);
+		}
+		
+		REQUIRE(log.transacting() == false);		
+	}
+	
+	SECTION("Test invalid_state exception") {
+		auto t = log.begin_transaction();
+		auto u = std::move(t);
+		REQUIRE_THROWS_AS(t.read_hash(), transaction_invalid_state);
+	}
 }
