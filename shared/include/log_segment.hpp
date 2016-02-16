11200ff4a8e61a28363fa9ea7c56a258c97f72f4
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-02-16 15:39:29 +0000

    Include a log_segment class
    This will represent a section of a log that can be serialised and
    inserted in another log

---
+++ shared/include/log_segment.hpp
@@ -0,0 +1,53 @@
+#ifndef LOG_SEGMENT_HPP
+#define LOG_SEGMENT_HPP
+
+#include "log_utility.hpp"
+
+
+class log_segment {
+public:
+	typedef log_utility utility_type;
+	typedef utility_type::container_type container_type;
+	
+	typedef utility_type::value_type value_type;
+	typedef value_type::header_type header_type;
+	
+	typedef utility_type::size_type size_type;
+	typedef value_type& reference;
+	typedef const reference const_reference;
+
+	
+	typedef utility_type::serial_type serial_type;
+	typedef utility_type::serial_ptr serial_ptr;
+	
+	typedef container_type::iterator iterator;
+	typedef container_type::const_iterator const_iterator;
+	
+public:
+	log_segment(hash512 hash, container_type&& segment);
+	log_segment(const log_segment& orig);
+	log_segment(const serial_ptr serial);
+	virtual ~log_segment();
+        
+	serial_ptr serialise();	
+	size_type log_size();
+	
+	size_type size();
+	const log_header& header();
+	
+	const_iterator cbegin();
+	const_iterator begin();
+	
+	const_iterator cend();
+	const_iterator end();
+	
+
+private:
+    container_type m_segment;
+	log_header m_header;
+
+	void deserialise(const serial_ptr serial);
+};
+
+#endif /* LOG_SEGMENT_HPP */
+
---
+++ shared/log_segment.cpp
@@ -0,0 +1,90 @@
+#include "log_utility.hpp"
+#include "log_segment.hpp"
+
+log_segment::log_segment(hash512 hash, container_type&& segment) {
+    m_header.hash = hash;
+    m_segment = std::move(segment);
+    m_header.num_records = m_segment.size();
+}
+
+log_segment::log_segment(const log_segment& orig) {
+    m_header = orig.m_header;
+    m_segment = orig.m_segment;
+}
+
+log_segment::log_segment(const serial_ptr serial) {
+	deserialise(serial);
+}
+
+log_segment::~log_segment() {
+}
+
+log_segment::serial_ptr log_segment::serialise() {
+    auto hdr_ptr = reinterpret_cast<serial_ptr>(&m_header);
+    auto hdr_sz = sizeof(log_header);
+
+    auto heap = new serial_type[log_size()];
+    auto ptr = heap;
+
+    std::copy(hdr_ptr, hdr_ptr + hdr_sz, ptr);
+    ptr += hdr_sz;
+
+    for(auto& r : m_segment) {
+            auto s = r.serialise();
+            std::copy(s, s + r.size(), ptr);
+            ptr += r.size();
+            delete s;
+    }
+
+    return heap;
+}
+
+log_segment::size_type log_segment::log_size() {
+    size_type sz = sizeof(log_header);
+    for(auto& r : m_segment) {
+            sz += r.size();
+    }
+    return sz;
+}
+
+log_segment::size_type log_segment::size() {
+    return m_segment.size();
+}
+
+const log_header& log_segment::header() {
+	return m_header;
+}
+
+log_segment::const_iterator log_segment::cbegin() {
+	return m_segment.cbegin();
+}
+
+log_segment::const_iterator log_segment::begin() {
+	return m_segment.cbegin();
+}
+
+
+log_segment::const_iterator log_segment::cend() {
+	return m_segment.cend();
+}
+
+log_segment::const_iterator log_segment::end() {
+	return m_segment.cend();
+}
+
+
+void log_segment::deserialise(const serial_ptr serial) {
+	auto hdr = reinterpret_cast<log_header*>(serial);
+	std::copy(hdr, hdr+1, &m_header);
+	auto ptr = serial + sizeof(log_header);
+	for(auto i = 0; i < m_header.num_records; i++) {
+		activity a;
+		a.deserialise(ptr);
+		ptr += a.size();
+		m_segment.push_back(a);
+	}
+}
+
+
+
+
