26c89fd162e0f89131559f7f575708740c9349da
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-02-24 18:02:59 +0000

    protocol_common: Added frames and constructors
    These can quickly construct specific frames for use with
    communicating requests and responses

--- shared/include/protocol/protocol_common.hpp
+++ shared/include/protocol/protocol_common.hpp
@@ -18,5 +18,30 @@ enum class dvsp_msgtype : char {
 
 	management_echo,
 };
+
+struct frame_register {
+	char type;
+	char len;
+	std::array<char, 62> hostname;
+};
+
+struct frame_response_code {
+	int response;
+};
+
+inline frame_register construct_frame_register(netnode_type type, std::string hostname) {
+	frame_register fr;
+
+	fr.type = static_cast<char>(type);
+	fr.len = hostname.length();
+	hostname.copy(fr.hostname.data(), hostname.length());
+	return fr;
+};
+
+inline frame_response_code construct_frame_response_code(int code) {
+	frame_response_code frc;
+	frc.response = code;
+	return frc;
+}
 #endif /* PROTOCOL_COMMON_HPP */
 
