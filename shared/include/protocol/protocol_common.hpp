1506a955558fb02862460717de6d76797b35d67d
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-02-26 15:16:25 +0000

    protocol: Include address response frame and ctor
    The response frame is packed for transmission and provides
    a response code that can be used for checking that the
    operation was successful

--- shared/include/protocol/protocol_common.hpp
+++ shared/include/protocol/protocol_common.hpp
@@ -39,6 +39,11 @@ struct frame_response_code {
 	dvsp_rcode response;
 };
 
+struct  __attribute__((packed)) frame_address {
+	dvsp_rcode response;
+	netspace_ipv4 addr;
+};
+
 inline frame_register construct_frame_register(netnode_type type, std::string hostname) {
 	frame_register fr;
 
@@ -52,6 +57,13 @@ inline frame_response_code construct_frame_response_code(dvsp_rcode code) {
 	frame_response_code frc;
 	frc.response = code;
 	return frc;
+}
+
+inline frame_address construct_frame_address(netspace_addr addr) {
+	frame_address f;
+	f.response = dvsp_rcode::ok;
+	f.addr = addr.to_v4().to_bytes();
+	return f;
 }
 #endif /* PROTOCOL_COMMON_HPP */
 
