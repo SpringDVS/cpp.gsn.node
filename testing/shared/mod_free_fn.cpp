d76ade9bd2e642c9ee289e4d8ff7278cf5b8ea88
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-02-27 12:56:04 +0000

    testing: Add test for ipv4_to_netspace_addr

---
+++ testing/shared/mod_free_fn.cpp
@@ -0,0 +1,9 @@
+#include "catch.hpp"
+
+#include "infrastructure/net_common.hpp"
+TEST_CASE("ipv4_to_netspace_addr", "[free]") {
+	netspace_ipv4 ipv4{{192,168,1,1}};
+	auto addr = ipv4_to_netspace_addr(ipv4);
+	
+	REQUIRE(addr.to_string() == "192.168.1.1");
+}
