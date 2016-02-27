c6eafb1624684de019789bc16a3fde6a382773e6
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-02-27 17:10:20 +0000

    client forge: Add handling for gsn_local_area
    This enables the client to send local_area probe
    requests to see other nodes in the local gsn and
    print the results

--- clients/forge/forge.cpp
+++ clients/forge/forge.cpp
@@ -13,11 +13,32 @@ void view_frame_response(packet_uptr packet) {
 		<< std::endl << std::endl;
 }
 
-void forge_gsn_register_host(netspace_addr target) {
+void view_frame_network(packet_uptr packet) {
+	auto frame = packet->content_as<frame_network>();
+	std::cout 
+		<< "Response: " << rcode_to_string(frame.response) 
+		<< std::endl;
+	
+	if(frame.response != rcode::ok) {
+		std::cout << std::endl;
+		return;
+	}
+	
+	for(auto i = 0; i < frame.total; i++) {
+		auto node = packet->content_as<netspace_ipv4>(sizeof(frame_network) + 1 + (4*i));
+		std::cout << ipv4_to_string(node) << std::endl;
+	}
+	
+	std::cout << std::endl;
+}
+
+void forge_gsn_register_host(netspace_addr target, std::string& hostname) {
 	packet_uptr p(new dvsp_packet);
 	p->header().addr_dest = target.to_v4().to_bytes();
 	p->header().size = 0;
 	p->header().type = msgtype::gsn_register_host;
+	auto frame = construct_frame_register(netnode_type::org, hostname);
+	p->copy_content(&frame, sizeof(frame_register));
 	auto out = dispatch_packet(std::move(p));
 	view_frame_response(std::move(out));
 }
@@ -31,22 +52,36 @@ void forge_gsn_unregister_host(netspace_addr target) {
 	view_frame_response(std::move(out));
 }
 
+void forge_gsn_local_area(netspace_addr target) {
+	packet_uptr p(new dvsp_packet);
+	p->header().addr_dest = target.to_v4().to_bytes();
+	p->header().size = 0;
+	p->header().type = msgtype::gsn_local_area;
+	auto out = dispatch_packet(std::move(p));
+	view_frame_network(std::move(out));
+}
+
 void run_forge(dvsp_msgtype type, netspace_addr target, std::string content) {
 	packet_uptr p;
 	switch(type) {
 		case msgtype::gsn_register_host:
 			std::cout << "Forging `gsn_register_host`... ";
-			forge_gsn_register_host(target);
+			forge_gsn_register_host(target, content);
 			break;
 		case msgtype::gsn_unregister_host:
 			std::cout << "Forging `gsn_unregister_host`... ";
 			forge_gsn_unregister_host(target);
 			break;
 			
-		default: break;
+		case msgtype::gsn_local_area:
+			std::cout << "Forging `gsn_local_area`... ";
+			forge_gsn_local_area(target);
+			break;
+			
+		default:
+			std::cout << "Unimplemented frame" << std::endl; 
+			break;
 	}
-	
-	content.clear();
 }
 
 std::string rcode_to_string(rcode code) {
@@ -71,6 +106,7 @@ packet_uptr dispatch_packet(packet_uptr packet) {
 	netspace_udp::socket socket(io_service);
 	socket.open(netspace_udp::v4());
 	
+	packet->header().addr_orig = socket.local_endpoint().address().to_v4().to_bytes();
 	auto ptr = packet->serialise();
 	
 	socket.send_to(boost::asio::buffer(ptr, packet->size()), rcv_ep);
