14f9c9c27f218b76675aa5776f1cb4dbf39c7ce1
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-03-02 01:51:59 +0000

    http_service: Move http processing to http_to_bin
    The function now handles the conversion, so the service
    just needs to pass back the final frame

--- nodes/root/http_service.hpp
+++ nodes/root/http_service.hpp
@@ -11,13 +11,13 @@ public:
 	http_service(const http_service&) = delete;
 	~http_service();
 	
-	static void send_frame(const http_frame& frame, netspace_ipv4 address){
+	static dvsp_packet send_frame(const http_frame& frame, netspace_ipv4 address){
 		char reply[4096];
 		try {
 		netspace_ios ios;
 		netspace_tcp::resolver resolver(ios);
 		auto nsaddr = ipv4_to_netspace_addr(address);
-		netspace_tcp::resolver::query query(netspace_tcp::v4(), nsaddr.to_string(), "80");
+		netspace_tcp::resolver::query query(netspace_tcp::v4(), nsaddr.to_string(), "8080");
 
 		auto it = resolver.resolve(query);
 
@@ -27,30 +27,13 @@ public:
 		boost::asio::write(s, boost::asio::buffer(serial.get(), frame.size()));
 		
 		s.read_some(boost::asio::buffer(reply, 4096));
-		//std::cout << reply << std::endl;
+		
 		std::string r(reply);
+		auto raw = http_to_bin(reply);
+		return dvsp_packet(raw.get());
 		
-		auto n = r.find("\r\n\r\n");
-		auto split = r.substr(n+4);
-		FILE* fp;
-		auto header = r.substr(0, n);
-		char bytes[1024];
-		auto i = 0;
-		for(auto j = 0u; j < split.length(); j++) {
-			char d[2];
-			d[0] = split[j++];
-			d[1] = split[j];
-			bytes[i++] = strtol(d, NULL,16);
-		}
-		dvsp_packet p(const_cast<char*>(bytes));
-		fp = fopen("response.bin", "wb");
-		fwrite(bytes, 1, i, fp );
-		fclose(fp);
-		
-		std::cout << p.header().size << std::endl;
 		} catch (std::exception& e) {
-			std::cout << "Error: " << e.what() << std::endl;
-			std::cout << reply << std::endl;
+			throw e;
 		}
 	};
 private:
